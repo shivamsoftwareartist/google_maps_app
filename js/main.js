19db1b2c4c7ee227ddc68871d9d284a4ca759432
Shivam Bansal <shivambansal157@gmail.com>
2020-04-22 23:18:44 +0530

    add new css js files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var map;
+var markers = [];
+var infoWindow;
+var locationSelect;
+
+function initMap() {
+	var sydney = {lat: -33.863276, lng: 151.107977};
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: sydney,
+		zoom: 11,
+		mapTypeId: 'roadmap',
+		mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
+	});
+}
